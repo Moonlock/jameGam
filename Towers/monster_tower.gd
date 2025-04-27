48631233aa0ef44640fd8a9dd0f5eee3516c37a5
Moonlock <>
2025-04-26 20:04:53 -0600

    Towers fire bullets

--- Towers/monster_tower.gd
+++ Towers/monster_tower.gd
@@ -1,1 +1,8 @@
 extends "res://Towers/towers.gd"
+
+@onready var bullet = preload("res://bullet.tscn")
+
+func _on_fire_rate_timer_timeout() -> void:
+	var bullet_instance = bullet.instantiate()
+	add_child(bullet_instance)
+	
--- bullet.gd
+++ bullet.gd
@@ -1,6 +1,7 @@
 class_name Bullet extends CharacterBody2D
 
 @export var _damage = 5
+@export var _speed = 400
 
 func damage():
 	return _damage
@@ -8,3 +9,6 @@ func damage():
 func handleHit():
 	print("Normal hit")
 	queue_free()
+
+func _physics_process(delta):
+	position += Vector2.RIGHT * delta  * _speed
